a9abfd5505e68595aea769dab3edbf6dc3eaba91
VickArmand <victormaina1962@gmail.com>
2024-04-20 21:38:37 +0300

    pycodestyle

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
-Write a type-annotated function concat
-that takes a string str1 and a string str2 as arguments
-and returns a concatenated string
+This module has function concat
 """
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Write a type-annotated function concat
+    that takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+    """
     return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import math
 """
-Write a type-annotated function floor which takes a float n
-as argument and returns the floor of the float.
+This module contains function floor
 """
 
 
 def floor(n: float) -> int:
+    """
+    Write a type-annotated function floor which takes a float n
+    as argument and returns the floor of the float.
+    """
     return math.floor(n)
