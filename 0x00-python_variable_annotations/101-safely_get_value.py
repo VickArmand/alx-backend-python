a895b6eea86ff53c50747801ff1e000024b0bd7d
VickArmand <victormaina1962@gmail.com>
2024-04-21 09:21:44 +0300

    pycodestyle

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,9 +3,9 @@
 This module has the function safely_get_value
 """
 from typing import Mapping, Any, Union, TypeVar
-
-
 T = TypeVar('T')
+
+
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
     """Return value of a key in dictionary"""
