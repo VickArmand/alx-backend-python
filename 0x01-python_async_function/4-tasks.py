054cf151f8f3da473f30e3a24b6a8739d595a1c4
VickArmand <victormaina1962@gmail.com>
2024-04-22 22:28:16 +0300

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module has a function task_wait_n
+"""
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n, max_delay):
+    """
+    Take the code from wait_n and alter it
+    into a new function task_wait_n.
+    The code is nearly identical to wait_n
+    except task_wait_random is being called.
+    """
+
+    res = [await task_wait_random(max_delay) for i in range(n)]
+    res.sort()
+    return res
